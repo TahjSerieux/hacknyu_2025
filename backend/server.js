d97deca4ac9db37d37d6a2f06b55035ae3b825c1
Tahj <Tahj.Serieux46@myhunter.cuny.edu>
2025-11-15 13:46:53 -0500

    Added paywall

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,13 @@ import express from 'express';
 
 const app = express()
 
+
 app.get('/',(req,res)=>{
     res.send({"message":"Hello, World"})
 })
+
+app.post('/chatbot/ask', (req,res)=>{
+    res.send({"Response":"Structured Data"})
+})
+
 app.listen(3004)
---
+++ frontend/my-app/app/paywall/page.tsx
@@ -0,0 +1,9 @@
+export default function PayWall(){
+
+    return(
+        <div>
+            <h1>Pay Up Buddy</h1>
+            <h2>Please dont ask me to center this T-T</h2>
+        </div>
+    )
+}
